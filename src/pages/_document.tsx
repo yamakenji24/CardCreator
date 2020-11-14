8747cc104614588149be19872d1fcfa967000e55
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-11-15 01:48:52 +0900

    renamed to ts or tsx

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="dns-prefetch" href="//www.google.co.jp" /> 
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  const initialProps = await Document.getInitialProps(ctx)
+  return { ...initialProps }
+};
