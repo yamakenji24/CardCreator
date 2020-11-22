0620440f14d8bd528eaaf8492c9c03c0ca683daf
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-11-23 03:14:12 +0900

    config/setting to use styled-component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,47 @@
-import '../../styles/globals.css'
+import React from 'react';
+import Head from 'next/head';
+import { AppProps } from 'next/app';
+import { createGlobalStyle, ThemeProvider} from 'styled-components';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <>
+      <Head>
+        <title>Card Creator</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
 
 export default MyApp
