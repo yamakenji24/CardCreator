51b1844ffab0c1d84ae7995a8add93c1a28394de
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-12-11 06:12:26 +0900

    adjust for tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,47 +1,18 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
-import { createGlobalStyle, ThemeProvider} from 'styled-components';
+import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
       <Head>
         <title>Card Creator</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
-      <GlobalStyle />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <Component {...pageProps} />
     </>
   )
 }
-
-const GlobalStyle = createGlobalStyle`
-  html,
-  body {
-    padding: 0;
-    margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  }
-
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-
-  * {
-    box-sizing: border-box;
-  }
-`
-
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
-
 
 export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
-import {ServerStyleSheet} from 'styled-components';
 
 export default class MyDocument extends Document {
   render() {
@@ -19,20 +18,12 @@ export default class MyDocument extends Document {
 }
 
 MyDocument.getInitialProps = async (ctx: DocumentContext) => {
-  const sheets = new ServerStyleSheet()
-  const originalRenderPage = ctx.renderPage;
-
-  ctx.renderPage = () => originalRenderPage({
-    enhanceApp: (App) => (props) => sheets.collectStyles(<App {...props} />),
-  })
-
   const initialProps = await Document.getInitialProps(ctx)
   return {
     ...initialProps, 
     styles: (
       <>
         {initialProps.styles}
-        {sheets.getStyleElement()}
       </>
     )
   }
