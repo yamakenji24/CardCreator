36813cde46f969efd7c9ff3a04ff6cb0c257d7ed
yamakenji24 <s20g486@stu.kagawa-u.ac.jp>
2020-12-17 18:26:07 +0900

    fix/improve layout

--- src/ui/card/cardlayout.tsx
+++ src/ui/card/cardlayout.tsx
@@ -3,29 +3,27 @@ import { useGetSession } from '../../utils/useGetSession';
 import { EmptyCardLayout } from './emptyCardProfile';
 import { Grass } from './grass';
 import { isNullOrUndefined } from 'util';
-import { checkProvider } from '../../utils/checkProvider';
 
 const color = 'bg-black';
 
 export const CardLayout = (): JSX.Element => {
   const {profileData} = useGetSession();
   if(isNullOrUndefined(profileData)) return <EmptyCardLayout color={color}/>
-  const boolProvider = checkProvider(profileData.provider)
   
   return (
-   <div className="w-crd h-80 my-40 mx-auto relative border-4 border-solid border-white p-1.5 rounded-3xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
+   <div className="w-crd h-80 my-20 mx-auto relative border-4 border-solid border-white p-1.5 rounded-3xl bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
       <div className={"w-full h-full rounded-3xl " + color}>
         <div className="flex">
           <img src="/logo.png" className="w-24 h-24 rounded-full" />
-          <Grass boolProvider={boolProvider} name={profileData.name} />
+          <Grass provider={profileData.provider} name={profileData.name} />
         </div>
         <Profile 
           name={profileData.name}
           picture={profileData.picture}
-          icon={boolProvider ? 'github' : 'google'} 
-          twitter={boolProvider ? profileData.profile.twitter : null}
+          provider={profileData.provider} 
+          twitter={profileData.twitter}
           email={profileData.email}
-          blog={boolProvider ? profileData.profile.blog : null}
+          blog={profileData.blog}
         />
       </div>
    </div>
